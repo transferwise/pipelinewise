ed4bd7bb4f375eb943c00dfef3673862294a4fb5
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2018-09-10 17:08:14 +0100

    CLI: discover and merge schema

--- cli/cli/etlwise.py
+++ cli/cli/etlwise.py
@@ -46,6 +46,13 @@ class ETLWise(object):
         self.tap_bin = self.get_connector_bin(self.tap["type"])
         self.target_bin = self.get_connector_bin(self.target["type"])
 
+    def is_json(self, string):
+        try:
+            json_object = json.loads(string)
+        except Exception as exc:
+            return False
+        return True
+
     def load_json(self, file):
         try:
             self.logger.debug('Parsing file at {}'.format(file))
@@ -56,6 +63,14 @@ class ETLWise(object):
                 return None
         except Exception as exc:
             raise Exception("Error parsing {} {}".format(file, exc))
+
+    def save_json(self, data, file):
+        try:
+            self.logger.info("Saving JSON {}".format(file))
+            with open(file, 'w') as f:
+                return json.dump(data, f, indent=2, sort_keys=True)
+        except Exception as exc:
+            raise Exception("Cannot save JSON {} {}".format(file, exc))
 
     def load_config(self):
         self.logger.debug('Loading config at {}'.format(self.config_path))
@@ -72,8 +87,6 @@ class ETLWise(object):
         return os.path.join(self.venv_dir, connector_type, "bin", connector_type)
   
     def get_connector_files(self, connector_dir):
-        name = os.path.basename(connector_dir)
-
         return {
             'config': os.path.join(connector_dir, 'config.json'),
             'properties': os.path.join(connector_dir, 'properties.json'),
@@ -135,7 +148,7 @@ class ETLWise(object):
         return tap
     
     def run_command(self, command, polling=False):
-        self.logger.debug('Running command with polling [{}] : {} with'.format(polling, command))
+        self.logger.info('Running command with polling [{}] : {}'.format(polling, command))
 
         if polling:
             proc = Popen(shlex.split(command), stdout=PIPE, stderr=STDOUT)
@@ -149,7 +162,7 @@ class ETLWise(object):
             
             rc = proc.poll()
             if rc != 0:
-                self.logger.warning(stdout)
+                self.logger.error(stdout)
                 sys.exit(rc)
             
             return [stdout, None]     
@@ -162,25 +175,129 @@ class ETLWise(object):
             stderr = x[1].decode('utf-8')
 
             if rc != 0:
-              self.logger.warning(stderr)
+              self.logger.error(stderr)
               sys.exit(rc)
             
             return [stdout, stderr]
+
+    def merge_schemas(self, old_schema, new_schema):
+        schema_with_diff = new_schema
+
+        if not old_schema:
+            schema_with_diff = new_schema
+        else:
+            new_streams = new_schema["streams"]
+            old_streams = old_schema["streams"]
+            for new_stream_idx, new_stream in enumerate(new_streams):
+                new_tap_stream_id = new_stream["tap_stream_id"]
+
+                old_stream = False
+                old_stream = next((item for item in old_streams if item["tap_stream_id"] == new_tap_stream_id), False)
+
+                # Is this a new stream?
+                if not old_stream:
+                    new_schema["streams"][new_stream_idx]["is-new"] = True
+
+                # Copy stream selection from the old properties
+                else:
+                    try:
+                        new_schema["streams"][new_stream_idx]["metadata"][0]["metadata"]["selected"] = old_stream["metadata"][0]["metadata"]["selected"]
+                        new_schema["streams"][new_stream_idx]["metadata"][0]["metadata"]["replication-method"] = old_stream["metadata"][0]["metadata"]["replication-method"]
+                    except Exception:
+                        False
+
+                # Is this new or modified field?
+                new_fields = new_schema["streams"][new_stream_idx]["schema"]["properties"]
+                old_fields = old_schema["streams"][new_stream_idx]["schema"]["properties"]
+                for new_field_key in new_fields:
+                    new_field = new_fields[new_field_key]
+                    new_field_mdata_idx = -1
+
+                    # Find new field metadata index
+                    for i, mdata in enumerate(new_schema["streams"][new_stream_idx]["metadata"]):
+                        if len(mdata["breadcrumb"]) == 2 and mdata["breadcrumb"][0] == "properties" and mdata["breadcrumb"][1] == new_field_key:
+                            new_field_mdata_idx = i
+
+                    # Field exists
+                    if new_field_key in old_fields.keys():
+                        old_field = old_fields[new_field_key]
+                        old_field_mdata_idx = -1
+
+                        # Find old field metadata index
+                        for i, mdata in enumerate(old_stream["metadata"]):
+                            if len(mdata["breadcrumb"]) == 2 and mdata["breadcrumb"][0] == "properties" and mdata["breadcrumb"][1] == new_field_key:
+                                old_field_mdata_idx = i
+
+                        # Copy field selection from the old properties
+                        try:
+                            new_schema["streams"][new_stream_idx]["metadata"][new_field_mdata_idx]["metadata"]["selected"] = old_stream["metadata"][old_field_mdata_idx]["metadata"]["selected"]
+                        except Exception:
+                            False
+
+                        # Field exists and type is the same - Do nothing more in the schema
+                        if new_field == old_field:
+                            self.logger.debug("Field exists in {} stream with the same type: {} : {}".format(new_tap_stream_id, new_field_key, new_field))
+
+                        # Field exists but types are different - Mark the field as modified in the metadata
+                        else:
+                            self.logger.debug("Field exists in {} stream but types are different: {} : {}".format(new_tap_stream_id, new_field_key, new_field))
+                            try:
+                                new_schema["streams"][new_stream_idx]["metadata"][new_field_mdata_idx]["metadata"]["is-modified"] = True
+                            except Exception:
+                                False
+
+                    # New field - Mark the field as new in the metadata
+                    else:
+                        self.logger.debug("New field in stream {}: {} : {}".format(new_tap_stream_id, new_field_key, new_field))
+                        try:
+                            new_schema["streams"][new_stream_idx]["metadata"][new_field_mdata_idx]["metadata"]["is-new"] = True
+                        except Exception:
+                            False
+
+            schema_with_diff = new_schema
+
+        return schema_with_diff
 
     def discover_tap(self):
         tap_id = self.tap["id"]
         tap_type = self.tap["type"]
         target_id = self.target["id"]
         target_type = self.target["type"]
+        old_schema_path = self.tap["files"]["properties"]
 
         self.logger.info("Discovering {} ({}) tap in {} ({}) target".format(tap_id, tap_type, target_id, target_type))
 
+        # Generate and run the command to run the tap directly
         tap_config = self.tap["files"]["config"]
         command = "{} --config {} --discover".format(self.tap_bin, tap_config)
-        schema = self.run_command(command, False)
+        result = self.run_command(command, False)
 
-        print("OUTPUT: {}".format(schema[0]))
-        print("STDERR: {}".format(schema[1]))
+        # Get output and errors from tap
+        new_schema, tap_output = result
+        self.logger.info("Tap output: {}".format(tap_output))
+
+        # Convert JSON string to object
+        try:
+            new_schema = json.loads(new_schema)
+        except Exception as exc:
+            self.logger.error("Schema discovered by {} ({}) is not a valid JSON.".format(tap_id, tap_type))
+            sys.exit(1)
+
+        # Merge the old and new schemas and diff changes
+        old_schema = self.load_json(old_schema_path)
+        if old_schema:
+            schema_with_diff = self.merge_schemas(old_schema, new_schema)
+        else :
+            schema_with_diff = new_schema
+
+        # Save the new catalog into the tap
+        try:
+            tap_properties_path = self.tap["files"]["properties"]
+            self.logger.info("Writing new properties file with changes into {}".format(tap_properties_path))
+            self.save_json(schema_with_diff, tap_properties_path)
+        except Exception as exc:
+            self.logger.error("Cannot save file. {}".format(str(exc)))
+            sys.exit(1)
 
     def run_tap(self):
         self.logger.info("Running {} tap in {} target".format(self.tap, self.target))
