3b0ce63a55c191dfe50c2b22b42855e06739af5a
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2018-09-29 10:10:16 +0100

    Don't use React FormattedMessage component in HTML option tag

--- admin-console/app/components/ReplicationMethodDropdown/index.js
+++ admin-console/app/components/ReplicationMethodDropdown/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
 
 import messages from './messages';
 
 function ReplicationMethodDropdown(props) {
   return (
     <select value={props.value} disabled={props.disabled} onChange={(event) => props.onChange(event.target.value)}>
-      <option key={`replication-method-full`} value="FULL_TABLE"><FormattedMessage {...messages.full} /></option>
-      <option key={`replication-method-incremental`} value="INCREMENTAL"><FormattedMessage {...messages.incremental} /></option>
-      <option key={`replication-method-log-based`} value="LOG_BASED"><FormattedMessage {...messages.logBased} /></option>
+      <option key={`replication-method-full`} value="FULL_TABLE">{messages.full.defaultMessage}</option>
+      <option key={`replication-method-incremental`} value="INCREMENTAL">{messages.incremental.defaultMessage}</option>
+      <option key={`replication-method-log-based`} value="LOG_BASED">{messages.logBased.defaultMessage}</option>
     </select>
   )
 }
