59a8f4cc8194701e01cf70ce6e5a7da46e9c2714
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2018-10-05 13:33:41 +0100

    ADMIN-CONSOLE: Respect select-by-default in tap-mysql

--- admin-console/app/singerConnectors/TapMysql/Properties.js
+++ admin-console/app/singerConnectors/TapMysql/Properties.js
@@ -169,7 +169,8 @@ export class TapMysqlProperties extends React.PureComponent {
     const stream = props.delegatedProps.stream;
     const streamId = props.delegatedProps.streamId;
     const isAutomatic = item.inclusion === 'automatic';
-    const isSelected = item.selected || isAutomatic;
+    const selectedByDefault = item.selectedByDefault;
+    const isSelected = item.selected || selectedByDefault || isAutomatic;
     const isUnsupported = item.inclusion === 'unsupported';
     let method = <FormattedMessage {...messages.notSelected} />
     const transformationType = item.transformationType;
