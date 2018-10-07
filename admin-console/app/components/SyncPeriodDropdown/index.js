2091b4b652ec5f4b100a89f3c09bf91902d5e7dd
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2018-10-07 13:28:37 +0100

    ADMIN-CONSOLE: Show custom schedules

--- admin-console/app/components/SyncPeriodDropdown/index.js
+++ admin-console/app/components/SyncPeriodDropdown/index.js
@@ -2,34 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import messages from './messages';
 
+const prettyCron = require('prettycron');
+
 function SyncPeriodDropdown(props) {
-  return (
-    <select className="form-control" value={props.value} disabled={props.disabled} onChange={(event) => props.onChange(event.target.value)}>
-      <option key={`sync-period-not-automated`} value="-1">{messages.syncPeriodNotAutomated.defaultMessage}</option>
-      <option key={`sync-period-cron-5-mins`} value="*/5 * * * *">{messages.syncPeriodCron5Mins.defaultMessage}</option>
-      <option key={`sync-period-cron-10-mins`} value="*/10 * * * *">{messages.syncPeriodCron10Mins.defaultMessage}</option>
-      <option key={`sync-period-cron-15-mins`} value="*/15 * * * *">{messages.syncPeriodCron15Mins.defaultMessage}</option>
-      <option key={`sync-period-cron-30-mins`} value="*/30 * * * *">{messages.syncPeriodCron30Mins.defaultMessage}</option>
-      <option key={`sync-period-cron-1-hour`} value="0 * * * *">{messages.syncPeriodCronHourly.defaultMessage}</option>
-      <option key={`sync-period-cron-2-hours`} value="0 */2 * * *">{messages.syncPeriodCron2Hours.defaultMessage}</option>
-      <option key={`sync-period-cron-3-hours`} value="0 */3 * * *">{messages.syncPeriodCron3Hours.defaultMessage}</option>
-      <option key={`sync-period-cron-6-hours`} value="0 */6 * * *">{messages.syncPeriodCron6Hours.defaultMessage}</option>
-      <option key={`sync-period-cron-12-hours`} value="0 */12 * * *">{messages.syncPeriodCron12Hours.defaultMessage}</option>
-      <option key={`sync-period-cron-daily`} value="0 0 * * *">{messages.syncPeriodCronDaily.defaultMessage}</option>
-      <option key={`sync-period-cron-mon`} value="0 0 * * MON">{messages.syncPeriodCronEveryMonday.defaultMessage}</option>
-      <option key={`sync-period-cron-tue`} value="0 0 * * TUE">{messages.syncPeriodCronEveryTuesday.defaultMessage}</option>
-      <option key={`sync-period-cron-wed`} value="0 0 * * WED">{messages.syncPeriodCronEveryWednesday.defaultMessage}</option>
-      <option key={`sync-period-cron-thu`} value="0 0 * * THU">{messages.syncPeriodCronEveryThursday.defaultMessage}</option>
-      <option key={`sync-period-cron-fri`} value="0 0 * * FRI">{messages.syncPeriodCronEveryFriday.defaultMessage}</option>
-      <option key={`sync-period-cron-sat`} value="0 0 * * SAT">{messages.syncPeriodCronEverySaturday.defaultMessage}</option>
-      <option key={`sync-period-cron-sun`} value="0 0 * * SUN">{messages.syncPeriodCronEverySunday.defaultMessage}</option>
-      <option key={`sync-period-cron-monthly-1-dom`} value="0 0 1 * *">{messages.syncPeriodCron1Mod.defaultMessage}</option>
-      <option key={`sync-period-cron-monthly-2-dom`} value="0 0 2 * *">{messages.syncPeriodCron2Mod.defaultMessage}</option>
-      <option key={`sync-period-cron-monthly-3-dom`} value="0 0 3 * *">{messages.syncPeriodCron3Mod.defaultMessage}</option>
-      <option key={`sync-period-cron-monthly-4-dom`} value="0 0 4 * *">{messages.syncPeriodCron4Mod.defaultMessage}</option>
-      <option key={`sync-period-cron-monthly-5-dom`} value="0 0 5 * *">{messages.syncPeriodCron5Mod.defaultMessage}</option>
-    </select>
-  )
+  const easySchedules = {
+    "-1": { name: messages.syncPeriodNotAutomated.defaultMessage },
+    "*/5 * * * *": { name: messages.syncPeriodCron5Mins.defaultMessage },
+    "*/10 * * * *": { name: messages.syncPeriodCron10Mins.defaultMessage },
+    "*/15 * * * *": { name: messages.syncPeriodCron15Mins.defaultMessage },
+    "*/30 * * * *": { name: messages.syncPeriodCron30Mins.defaultMessage },
+    "0 * * * *": { name: messages.syncPeriodCronHourly.defaultMessage },
+    "0 */2 * * *": { name: messages.syncPeriodCron2Hours.defaultMessage },
+    "0 */3 * * *": { name: messages.syncPeriodCron3Hours.defaultMessage },
+    "0 */6 * * *": { name: messages.syncPeriodCron6Hours.defaultMessage },
+    "0 */12 * * *": { name: messages.syncPeriodCron12Hours.defaultMessage },
+    "0 0 * * *": { name: messages.syncPeriodCronDaily.defaultMessage },
+    "0 0 * * MON": { name: messages.syncPeriodCronEveryMonday.defaultMessage },
+    "0 0 * * TUE": { name: messages.syncPeriodCronEveryTuesday.defaultMessage },
+    "0 0 * * WED": { name: messages.syncPeriodCronEveryWednesday.defaultMessage },
+    "0 0 * * THU": { name: messages.syncPeriodCronEveryThursday.defaultMessage },
+    "0 0 * * FRI": { name: messages.syncPeriodCronEveryFriday.defaultMessage },
+    "0 0 * * SAT": { name: messages.syncPeriodCronEverySaturday.defaultMessage },
+    "0 0 * * SUN": { name: messages.syncPeriodCronEverySunday.defaultMessage },
+    "0 0 1 * *": { name: messages.syncPeriodCron1Mod.defaultMessage },
+    "0 0 2 * *": { name: messages.syncPeriodCron2Mod.defaultMessage },
+    "0 0 3 * *": { name: messages.syncPeriodCron3Mod.defaultMessage },
+    "0 0 4 * *": { name: messages.syncPeriodCron4Mod.defaultMessage },
+    "0 0 5 * *": { name: messages.syncPeriodCron5Mod.defaultMessage },
+  }
+  if (easySchedules[props.value]) {
+    return (
+      <select className="form-control" value={props.value} disabled={props.disabled} onChange={(event) => props.onChange(event.target.value)}>
+        {Object.keys(easySchedules).map((k, i) => <option key={`sync-period-${i}`} value={k}>{easySchedules[k].name}</option>)}
+      </select>
+    )
+  } else {
+    return (
+      <div>
+        <select className="form-control" value={props.value} disabled={true} onChange={(event) => props.onChange(event.target.value)}>
+          <option key="sync-period-custom" value={props.value}>{props.value}</option>)}
+        </select>
+        {prettyCron.toString(props.value)}
+      </div>
+    )
+  }
 }
 
 SyncPeriodDropdown.propTypes = {
