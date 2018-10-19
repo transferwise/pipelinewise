cc81a62e78ac48d0ba41ed7b21902ac88f3ad69d
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2018-10-19 16:59:59 +0100

    Don't use schema name in index name

--- singer-connectors/target-postgres/target_postgres/db_sync.py
+++ singer-connectors/target-postgres/target_postgres/db_sync.py
@@ -280,7 +280,8 @@ class DbSync:
     def create_index(self, schema, index):
         if 'table' in index and 'columns' in index:
             table = self.table_name(index['table'], False)
-            index_name = 'idx_{}_{}'.format(table[:15], index['columns'].replace(',', '_'))
+            table_without_schema = self.table_name(index['table'], False, True)
+            index_name = 'idx_{}_{}'.format(table_without_schema[:15], index['columns'].replace(',', '_'))
             query = "CREATE INDEX IF NOT EXISTS {} ON {} ({})".format(index_name, table, index['columns'])
             logger.info("Creating index on '{}' table on '{}' column(s)... {}".format(table, index['columns'], query))
             self.query(query)
