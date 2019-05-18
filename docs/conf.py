88f65de0800e420cfa1475486cc487ab7965144e
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-05-18 11:06:04 +0100

    [doc] sphinx backward compatibility under 2.0 and  python 2.7

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The document name of the “master” document, that is, the document that
+# contains the root toctree directive. Default is 'index'.
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
