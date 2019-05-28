4bd3586ab418c1d7ebc156c5126722d9b5feb6ce
Louis Pieterse <louis.pieterse@transferwise.com>
2019-05-28 10:55:12 +0100

    mac compat

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Exit script on first error
+set -e
 
 # Ubuntu prerequisites
 # apt install python3 python3-pip python3-venv libpq-dev libsnappy-dev -y
@@ -12,7 +15,7 @@ VENV_DIR=$(pwd)/.virtualenvs
 python3 -m pip install --upgrade pip
 
 make_virtualenv() {
-    echo ""
+    echo
     echo "--------------------------------------------------------------------------"
     echo "Making Virtual Environment for $1"
     echo "--------------------------------------------------------------------------"
@@ -41,16 +44,6 @@ install_cli() {
     make_virtualenv cli
 }
 
-install_rest_api() {
-    cd $SRC_DIR/rest-api
-    make_virtualenv rest-api
-}
-
-install_admin_console() {
-    cd $SRC_DIR/admin-console
-    npm cache clean --force
-    npm run setup
-}
 
 # Install Singer connectors
 install_connector tap-mysql
@@ -71,20 +64,11 @@ install_fastsync postgres-to-snowflake
 # Install CLI
 install_cli
 
-# Install REST API
-#install_rest_api
-
-# Install web frontent
-#install_admin_console
-
 echo "--------------------------------------------------------------------------"
 echo "PipelineWise installed successfully"
 echo "--------------------------------------------------------------------------"
 echo
-echo "To start REST API:"
-echo "> source .virtualenvs/rest-api/bin/activate && cd rest-api && export FLASK_APP=rest_api && export FLASK_DEBUG=1 && flask run"
-echo
-echo "To start Web Interface:"
-echo "> cd admin-console && npm run start"
+echo "To start CLI:"
+echo "> source .virtualenvs/cli/bin/activate"
 echo
 echo "--------------------------------------------------------------------------"
