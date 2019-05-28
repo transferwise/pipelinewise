5a78917fe5e1027e94533c8364f5e39f5eae75b4
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-05-28 14:26:07 +0100

    Removed GUI and REST API

--- admin-console/app/containers/Taps/AddTap/constants.js
+++
@@ -1,6 +0,0 @@
-export const ADD_TAP = 'AddTap/ADD_TAP';
-export const ADD_TAP_SUCCESS = 'AddTap/ADD_TAP_SUCCESS';
-export const ADD_TAP_ERROR = 'AddTap/ADD_TAP_ERROR';
-
-export const SET_SUCCESS = 'AddTap/SET_SUCCESS';
-export const SET_ADD_TAP_BUTTON_STATE = 'AddTap/SET_ADD_TAP_BUTTON_STATE';
