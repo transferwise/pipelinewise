8c10a7ee683aca6e85028559311a47b083a817bc
Louis Pieterse <louis.pieterse@transferwise.com>
2019-06-07 09:34:43 +0100

    Add _SDC columns to fastsync postgres->snowflake

--- fastsync/postgres-to-snowflake/postgres_to_snowflake/postgres.py
+++ fastsync/postgres-to-snowflake/postgres_to_snowflake/postgres.py
@@ -174,7 +174,12 @@ class Postgres:
         if len(column_safe_sql_values) == 0:
             raise Exception("{} table not found.".format(table_name))
 
-        sql = "COPY (SELECT {}, now(), null, now() FROM {}) TO STDOUT with CSV DELIMITER ','".format(','.join(column_safe_sql_values), table_name)
+        sql = """COPY (SELECT {}
+        ,now()
+        ,null
+        ,now()
+        FROM {}) TO STDOUT with CSV DELIMITER ','
+        """.format(','.join(column_safe_sql_values), table_name)
         utils.log("POSTGRES - Exporting data: {}".format(sql))
         with gzip.open(path, 'wt') as gzfile:
             self.curr.copy_expert(sql, gzfile)
