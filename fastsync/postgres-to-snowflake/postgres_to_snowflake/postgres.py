bc2af279bd7f6fb1c8f52224b86220e6fe67b9a5
Louis Pieterse <louis.pieterse@transferwise.com>
2019-06-29 18:51:46 +0100

    Improve PostgreSQL version logic

--- fastsync/postgres-to-snowflake/postgres_to_snowflake/postgres.py
+++ fastsync/postgres-to-snowflake/postgres_to_snowflake/postgres.py
@@ -80,12 +80,13 @@ class Postgres:
                 else:
                     return []
 
+
     def fetch_current_log_sequence_number(self):
         # Make sure PostgreSQL version is 9.4 or higher
         result = self.query("SELECT setting::int AS version FROM pg_settings WHERE name='server_version_num'")
         version = result[0].get("version")
         if version < 90400 :
-                    raise Exception('Unable to use logical replication on PostgreSQL version {}'.format(version))
+            raise Exception('Unable to use logical replication on PostgreSQL version {}'.format(version))
 
         # Create replication slot, ignore error if already exists
         try:
@@ -94,7 +95,12 @@ class Postgres:
             pass
 
         # Get current lsn
-        result = self.query("SELECT pg_current_wal_lsn() AS current_lsn")
+        if version >= 100000:
+            result = self.query("SELECT pg_current_wal_lsn() AS current_lsn")
+        elif version >= 90400:
+            result = self.query("SELECT pg_current_xlog_location() AS current_lsn")
+        else:
+            raise Exception('Unable to use logical replication on PostgreSQL version {}'.format(version))
         current_lsn = result[0].get("current_lsn")
         file, index = current_lsn.split('/')
         return (int(file, 16)  << 32) + int(index, 16)
