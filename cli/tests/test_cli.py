afe0421ce55faf0cd3013429b04c4875f5808897
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-07-08 12:45:37 +0100

    add unit tests

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -1,3 +1,7 @@
+import os
+import pytest
+
+import cli.utils
 from cli_args import CliArgs
 from cli.pipelinewise import PipelineWise
 
@@ -17,36 +21,39 @@ class TestCli(object):
 
     def test_target_dir(self):
         """Singer target connector config path must be relative to the project config dir"""
-        assert (
-            self.pipelinewise.get_target_dir("dummy-target"),
-            "{}/dummy-target".format(CONFIG_DIR))
+        assert \
+            self.pipelinewise.get_target_dir("dummy-target") == \
+            "{}/dummy-target".format(CONFIG_DIR)
 
 
     def test_tap_dir(self):
         """Singer tap connector config path must be relative to the target connector config path"""
-        assert (
-            self.pipelinewise.get_tap_dir("dummy-target", "dummy-tap"),
-            "{}/dummy-target/dummy-tap".format(CONFIG_DIR))
+        assert \
+            self.pipelinewise.get_tap_dir("dummy-target", "dummy-tap") == \
+            "{}/dummy-target/dummy-tap".format(CONFIG_DIR)
 
 
     def test_tap_log_dir(self):
         """Singer tap log path must be relative to the tap connector config path"""
-        assert(
-            self.pipelinewise.get_tap_log_dir("dummy-target", "dummy-tap"),
-            "{}/dummy-target/dummy-tap/log".format(CONFIG_DIR))
+        assert \
+            self.pipelinewise.get_tap_log_dir("dummy-target", "dummy-tap") == \
+            "{}/dummy-target/dummy-tap/log".format(CONFIG_DIR)
 
 
     def test_connector_bin(self):
         """Singer connector binary must be at a certain location under PIPELINEWISE_HOME .virtualenvs dir"""
-        assert(
-            self.pipelinewise.get_connector_bin("dummy-type"),
-            "{}/dummy-type/bin/dummy-type".format(VIRTUALENVS_DIR))
+        assert \
+            self.pipelinewise.get_connector_bin("dummy-type") == \
+            "{}/dummy-type/bin/dummy-type".format(VIRTUALENVS_DIR)
 
 
     def test_connector_files(self):
         """Every singer connector must have a list of JSON files at certain locations"""
-        assert(
-            self.pipelinewise.get_connector_files("/var/singer-connector"),
+
+        # TODO: get_connector_files is duplicated in config.py and pipelinewise.py
+        #       Refactor to use only one
+        assert \
+            self.pipelinewise.get_connector_files("/var/singer-connector") == \
             {
                 'config': '/var/singer-connector/config.json',
                 'inheritable_config': '/var/singer-connector/inheritable_config.json',
@@ -54,4 +61,80 @@ class TestCli(object):
                 'state': '/var/singer-connector/state.json',
                 'transformation': '/var/singer-connector/transformation.json',
                 'selection': '/var/singer-connector/selection.json'
-            })
+            }
+
+
+    def test_create_consumable_target_config(self):
+        """Test merging target config.json and inheritable_config.json"""
+        target_config = "{}/resources/target-config.json".format(os.path.dirname(__file__))
+        tap_inheritable_config = "{}/resources/tap-inheritable-config.json".format(os.path.dirname(__file__))
+
+        # The merged JSON written into a temp file
+        temp_file = self.pipelinewise.create_consumable_target_config(target_config, tap_inheritable_config)
+        cons_targ_config = cli.utils.load_json(temp_file)
+
+        # The merged object needs
+        assert cons_targ_config == {
+            "account": "foo",
+            "aws_access_key_id": "secret",
+            "aws_secret_access_key": "secret/",
+            "client_side_encryption_master_key": "secret=",
+            "dbname": "my_db",
+            "file_format": "my_file_format",
+            "password": "secret",
+            "s3_bucket": "foo",
+            "s3_key_prefix": "foo/",
+            "stage": "my_stage",
+            "user": "user",
+            "warehouse": "MY_WAREHOUSE",
+            "batch_size_rows": 5000,
+            "data_flattening_max_level": 0,
+            "default_target_schema": "jira_clear",
+            "default_target_schema_select_permissions": [
+                "grp_power"
+            ],
+            "hard_delete": True,
+            "primary_key_required": True,
+            "schema_mapping": {
+                "jira": {
+                    "target_schema": "jira_clear",
+                    "target_schema_select_permissions": [
+                        "grp_power"
+                    ]
+                }
+            }
+        }
+
+        # Remove temp file with merged JSON
+        os.remove(temp_file)
+
+
+    def test_invalid_create_consumable_target_config(self):
+        """Test merging invalid target config.json and inheritable_config.json"""
+        target_config = "{}/resources/invalid.json".format(os.path.dirname(__file__))
+        tap_inheritable_config = "not-existing-json"
+
+        # Merging invalid or not existing JSONs should raise exception
+        with pytest.raises(Exception):
+            self.pipelinewise.create_consumable_target_config(target_config, tap_inheritable_config)
+
+
+
+    def test_tap_properties(self):
+        """Test tap property getter functions"""
+        assert 1 == 1
+
+
+    def test_encrypt_string(self, capsys):
+        """Test vault encryption command output"""
+        secret_path = "{}/resources/vault-secret.txt".format(os.path.dirname(__file__))
+
+        args = CliArgs(string="plain text", secret=secret_path)
+        pipelinewise = PipelineWise(args, CONFIG_DIR, VIRTUALENVS_DIR)
+
+        # Encrypted string should be printed to stdout
+        pipelinewise.encrypt_string()
+        stdout, stderr = capsys.readouterr()
+        assert not stderr.strip()
+        assert stdout.startswith("!vault |") and "$ANSIBLE_VAULT;" in stdout
+
--- cli/tests/test_utils.py
+++ cli/tests/test_utils.py
@@ -21,6 +21,8 @@ class TestUtils(object):
 
         assert cli.utils.is_json_file("./dummy-json") == False
         assert cli.utils.is_json_file("{}/resources/example.json".format(os.path.dirname(__file__))) == True
+        assert cli.utils.is_json_file("{}/resources/invalid.json".format(os.path.dirname(__file__))) == False
+        assert cli.utils.is_json_file("{}/resources".format(os.path.dirname(__file__))) == False
 
 
     def test_json_loader(self):
@@ -33,8 +35,8 @@ class TestUtils(object):
             cli.utils.load_json("{}/resources/invalid.json".format(os.path.dirname(__file__)))
 
         # Loading JSON should return python dict
-        assert (
-            cli.utils.load_json("{}/resources/example.json".format(os.path.dirname(__file__))),
+        assert \
+            cli.utils.load_json("{}/resources/example.json".format(os.path.dirname(__file__))) == \
             {
                 "glossary": {
                     "title": "example glossary",
@@ -56,12 +58,29 @@ class TestUtils(object):
                         }
                     }
                 }
-            })
+            }
+
+
+    def test_json_saver(self):
+        """Testing JSON save functions"""
+        obj = {"foo": "bar"}
+        # Saving to invalid path should raise exception
+        with pytest.raises(Exception):
+            cli.utils.save_json(obj, "/invalid/path")
+
+        # Saving and reloading should match
+        cli.utils.save_json(obj, "test-json.json")
+        assert cli.utils.load_json("test-json.json") == obj
+
+        # Delete output file, it's not required
+        os.remove("test-json.json")
 
 
     def test_yaml_detectors(self):
         """Testing YAML detector functions"""
-        assert cli.utils.is_json("{Invalid YAML}") == False
+        assert cli.utils.is_yaml("""
+            foo:
+            -bar""") == False
 
         assert cli.utils.is_yaml("id: 123") == True
         assert cli.utils.is_yaml("""
@@ -73,6 +92,8 @@ class TestUtils(object):
 
         assert cli.utils.is_yaml_file("./dummy-yaml") == False
         assert cli.utils.is_yaml_file("{}/resources/example.yml".format(os.path.dirname(__file__))) == True 
+        assert cli.utils.is_yaml_file("{}/resources/invalid.yml".format(os.path.dirname(__file__))) == False
+        assert cli.utils.is_yaml_file("{}/resources".format(os.path.dirname(__file__))) == False
 
 
     def test_yaml_loader(self):
@@ -84,23 +105,35 @@ class TestUtils(object):
         with pytest.raises(Exception):
             cli.utils.load_yaml("{}/resources/invalid.yml".format(os.path.dirname(__file__)))
 
-        assert (
-            cli.utils.load_yaml("{}/resources/example.yml".format(os.path.dirname(__file__))),
-            ['Apple', 'Orange', 'Strawberry', 'Mango'])
+        # Loading JSON file with valid JSON syntax but invalid vault secret file should raise exception
+        with pytest.raises(Exception):
+            cli.utils.load_json("{}/resources/example.json".format(os.path.dirname(__file__)), "invalid-secret-file-path")
+
+        # Loading valid YAML file with no vault encryption
+        assert \
+            cli.utils.load_yaml("{}/resources/example.yml".format(os.path.dirname(__file__))) == \
+            ['Apple', 'Orange', 'Strawberry', 'Mango']
+
+        # Loading valid YAML file with vault encrypted properties
+        assert \
+            cli.utils.load_yaml(
+                "{}/resources/example-with-vault.yml".format(os.path.dirname(__file__)),
+                "{}/resources/vault-secret.txt".format(os.path.dirname(__file__))) == \
+            ['Apple', 'Orange', 'Strawberry', 'Mango', 'Vault Encrypted Secret Fruit']
 
 
     def test_sample_file_path(self):
         """Sample files must be a tap, target YAML or README file"""
         for sample in cli.utils.get_sample_file_paths():
             assert os.path.isfile(sample) == True
-            assert (
-                re.match(".*[tap|target]_.*.yml.sample$", sample) or re.match(".*README.md$", sample))
+            assert \
+                re.match(".*(tap|target)_.*.yml.sample$", sample) or re.match(".*README.md$", sample)
 
 
     def test_extract_log_attributes(self):
         """Log files must match to certain pattern with embedded attributes in the file name"""
-        assert (
-            cli.utils.extract_log_attributes("snowflake-fx-20190508_000038.singer.log.success"),
+        assert \
+            cli.utils.extract_log_attributes("snowflake-fx-20190508_000038.singer.log.success") == \
             {
                 'filename': 'snowflake-fx-20190508_000038.singer.log.success',
                 'target_id': 'snowflake',
@@ -108,10 +141,10 @@ class TestUtils(object):
                 'timestamp': '2019-05-08T00:00:38',
                 'sync_engine': 'singer',
                 'status': 'success'
-            })
+            }
 
-        assert (
-            cli.utils.extract_log_attributes("snowflake-fx-20190508_231238.fastsync.log.running"),
+        assert \
+            cli.utils.extract_log_attributes("snowflake-fx-20190508_231238.fastsync.log.running") == \
             {
                 'filename': 'snowflake-fx-20190508_231238.fastsync.log.running',
                 'target_id': 'snowflake',
@@ -119,10 +152,10 @@ class TestUtils(object):
                 'timestamp': '2019-05-08T23:12:38',
                 'sync_engine': 'fastsync',
                 'status': 'running'
-            })
+            }
 
-        assert (
-            cli.utils.extract_log_attributes("dummy-log-file.log"),
+        assert \
+            cli.utils.extract_log_attributes("dummy-log-file.log") == \
             {
                 'filename': 'dummy-log-file.log',
                 'target_id': 'unknown',
@@ -130,11 +163,155 @@ class TestUtils(object):
                 'timestamp': '1970-01-01T00:00:00',
                 'sync_engine': 'unknown',
                 'status': 'unknown'
-            })
+            }
         
 
     def test_fastsync_bin(self):
-        """..."""
-        assert (
-            cli.utils.get_fastsync_bin(VIRTUALENVS_DIR, 'mysql', 'snowflake'),
-            "{}/mysql-to-snowflake/bin/mysql-to-snowflake".format(VIRTUALENVS_DIR))
+        """Fastsync binary paths must point to certain virtual environments"""
+        # Giving tap and target types should be enough to generate full path to fastsync binaries
+        assert \
+            cli.utils.get_fastsync_bin(VIRTUALENVS_DIR, 'mysql', 'snowflake') == \
+            "{}/mysql-to-snowflake/bin/mysql-to-snowflake".format(VIRTUALENVS_DIR)
+
+
+    def test_vault(self):
+        """Test vault encrypt and decrypt functionalities"""
+        # Encrypting with not existing file with secret should exit
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            cli.utils.vault_encrypt("plain_test", "not-existing-secret-file")
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 1
+
+        # Encrypted string should start with $ANSIBLE_VAULT; identifier
+        encrypted_str = str(cli.utils.vault_encrypt(
+                "plain_text",
+                "{}/resources/vault-secret.txt".format(os.path.dirname(__file__))))
+        assert encrypted_str.startswith("b'$ANSIBLE_VAULT;") == True
+
+        # Formatted encrypted string should start with special token and should keep the original vault encrypted value
+        formatted_encrypted_str = cli.utils.vault_format_ciphertext_yaml(encrypted_str)
+        assert formatted_encrypted_str.startswith("!vault |") and "b'$ANSIBLE_VAULT;" in formatted_encrypted_str
+
+        # Optional name argument should add the name to the output string as a key
+        formatted_encrypted_str = cli.utils.vault_format_ciphertext_yaml(encrypted_str, name="encrypted_plain_text")
+        assert formatted_encrypted_str.startswith("encrypted_plain_text: !vault |") and "b'$ANSIBLE_VAULT;" in formatted_encrypted_str
+
+
+    def test_schema_loader(self):
+        """Test JSON Schema loader functions"""
+        # Loading JSON schema file that not exist should exit
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            cli.utils.load_schema("/invalid/location/to/schema") is None
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 1
+
+        # Loading existing JSON schema should be loaded correctly
+        tap_schema = cli.utils.load_json("{}/../cli/schemas/tap.json".format(os.path.dirname(__file__)))
+        assert cli.utils.load_schema("tap") == tap_schema
+
+
+    def test_json_validate(self):
+        """Test JSON schema validator functions"""
+        schema = cli.utils.load_schema("tap")
+
+        # Valid instance should return None
+        valid_tap = cli.utils.load_yaml("{}/resources/tap-valid-mysql.yml".format(os.path.dirname(__file__)))
+        assert cli.utils.validate(valid_tap, schema) is None
+
+        # Invalid instance should exit
+        invalid_tap = cli.utils.load_yaml("{}/resources/tap-invalid.yml".format(os.path.dirname(__file__)))
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            cli.utils.validate(invalid_tap, schema)
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 1
+
+
+    def test_delete_keys(self):
+        """Test dictionary functions"""
+        # Delete single key with empty value
+        assert cli.utils.delete_empty_keys(
+            {"foo": "bar", "foo2": None}) == {"foo": "bar"}
+
+        # Delete multiple keys with empty value
+        assert cli.utils.delete_empty_keys(
+            {"foo": "bar", "foo2": None, "foo3": None, "foo4": "bar4"}) == {"foo": "bar", "foo4": "bar4"}
+
+        # Delete single key by name
+        assert cli.utils.delete_keys_from_dict(
+            {"foo": "bar", "foo2": "bar2"}, ["foo2"]) == {"foo": "bar"}
+
+        # Delete single key by name
+        assert cli.utils.delete_keys_from_dict(
+            {"foo": "bar", "foo2": "bar2", "foo3": None, "foo4": "bar4"},["foo2", "foo4"]) == {"foo": "bar", "foo3": None}
+
+        # Delete multiple keys from list of nested dictionaries
+        assert cli.utils.delete_keys_from_dict(
+            [{"foo": "bar", "foo2": "bar2"},
+             {"foo3": {"nested_foo": "nested_bar", "nested_foo2": "nested_bar2"}}], ["foo2", "nested_foo"]) == \
+             [{"foo": "bar"},
+              {"foo3": {"nested_foo2": "nested_bar2"}}]
+
+
+    def test_silentremove(self):
+        """Test removing functions"""
+        # Deleting non existing file should not raise exception
+        assert cli.utils.silentremove("this-file-not-exists.json") is None
+
+
+    def test_tap_properties(self):
+        """Test tap property getter functions"""
+        tap = cli.utils.load_yaml("{}/resources/tap-valid-mysql.yml".format(os.path.dirname(__file__)))
+
+        # Every tap should have catalog argument --properties or --catalog
+        tap_catalog_argument = cli.utils.get_tap_property(tap, "tap_catalog_argument")
+        assert tap_catalog_argument == "--catalog" or tap_catalog_argument == "--properties"
+
+        # Every tap should have extra_config_keys defined in dict
+        assert isinstance(cli.utils.get_tap_extra_config_keys(tap), dict) == True
+
+        # MySQL stream_id should be formatted as {{schema_name}}-{{table_name}}
+        assert cli.utils.get_tap_stream_id(tap, "dummy_db", "dummy_schema", "dummy_table") == "dummy_schema-dummy_table"
+
+        # MySQL stream_name should be formatted as {{schema_name}}-{{table_name}}
+        assert cli.utils.get_tap_stream_name(tap, "dummy_db", "dummy_schema", "dummy_table") == "dummy_schema-dummy_table"
+
+        # MySQL stream_name should be formatted as {{schema_name}}-{{table_name}}
+        assert cli.utils.get_tap_default_replication_method(tap) == "LOG_BASED"
+
+        # Get property value by tap type
+        assert cli.utils.get_tap_property_by_tap_type("tap-mysql", "default_replication_method") == "LOG_BASED"
+
+
+    def test_run_command(self):
+        """Test run command functions
+
+            Run command runs everything enclosed by /bin/bash -o pipefail -c '{}'
+            This means arguments should pass as plain string after the command
+
+            Return value is an array of: [return_code, stdout, stderr]
+        """
+
+        # Printing something to stdout should return 0
+        [rc, stdout, stderr] =  cli.utils.run_command("echo this is a test line")
+        assert [rc, stdout, stderr] == [0, "this is a test line\n", ""]
+
+        # Running an invalid command should return 127 and some error message to stdout
+        [rc, stdout, stderr] = cli.utils.run_command("invalid-command this is an invalid command")
+        assert [rc, stdout] == [127, ""]
+        assert stderr != ""
+
+        # If loggin enabled then a success command should create log file with success status
+        [rc, stdout, stderr] = cli.utils.run_command("echo this is a test line", log_file="./test.log")
+        assert [rc, stdout, stderr] == [0, "this is a test line\n", None]
+        os.path.isfile("test.log.success")
+        os.remove("test.log.success")
+
+        # If loggin enabled then a failed command should create log file with failed status
+        # NOTE: When logging is enabled and the command fails then it raises an exception
+        #       This behaviour is not in sync with no logging option
+        # TODO: Sync failed command execution behaviour with logging and no-logging option
+        #       Both should return [rc, stdout, stderr] list or both should raise exception
+        with pytest.raises(Exception):
+            [rc, stdout, stderr] = cli.utils.run_command("invalid-command this is an invalid command", log_file="./test.log")
+        os.path.isfile("test.log.failed")
+        os.remove("test.log.failed")
