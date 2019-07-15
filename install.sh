7a3a522e041bc73e4f992e1b08929bf5cffee7ae
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-07-15 12:57:23 +0100

    check licenses

--- install.sh
+++ install.sh
@@ -16,11 +16,44 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PIPELINEWISE_HOME=$(pwd)
 VENV_DIR=${PIPELINEWISE_HOME}/.virtualenvs
 
-make_virtualenv() {
+check_license() {
+    pip install pip-licenses
+
     echo
-    echo "--------------------------------------------------------------------------"
+    echo "Checking license..."
+    PKG_NAME=`pip-licenses | grep $1 | awk '{print $1}'`
+    PKG_VERSION=`pip-licenses | grep $1 | awk '{print $2}'`
+    PKG_LICENSE=`pip-licenses --from mixed | grep $1 | awk '{for (i=1; i<=NF-2; i++) $i = $(i+2); NF-=2; print}'`
+
+    # Any License Agreement that is not MIT has to be accepted
+    if [[ $PKG_LICENSE != "MIT License" && $PKG_LICENSE != 'UNKNOWN' ]]; then
+        echo
+        echo "  | $PKG_NAME ($PKG_VERSION) is licensed under $PKG_LICENSE"
+        echo "  |"
+        echo "  | WARNING. The license of this connector is different than the default PipelineWise license (MIT)."
+
+        if [[ $ACCEPT_LICENSES != "YES" ]]; then
+            echo "  | You need to accept the connector's license agreement to proceed."
+            echo "  |"
+            read -r -p "  | Do you accept the [$PKG_LICENSE] license agreement of $PKG_NAME connector? [y/N] " response
+            case "$response" in
+                [yY][eE][sS]|[yY])
+                    ;;
+                *)
+                    echo
+                    echo "EXIT. License agreement not accepted"
+                    exit 1
+                    ;;
+            esac
+        else
+            echo "  | You automatically accepted this license agreement by running this script with --acceptlicenses option."
+        fi
+
+    fi
+}
+
+make_virtualenv() {
     echo "Making Virtual Environment for $1"
-    echo "--------------------------------------------------------------------------"
     python3 -m venv $VENV_DIR/$1
     source $VENV_DIR/$1/bin/activate
     python3 -m pip install --upgrade pip
@@ -30,10 +63,16 @@ make_virtualenv() {
     if [ -f "setup.py" ]; then
         python3 -m pip install .
     fi
+
+    check_license $1
     deactivate
 }
 
 install_connector() {
+    echo
+    echo "--------------------------------------------------------------------------"
+    echo "Insalling $1 connector..."
+    echo "--------------------------------------------------------------------------"
     cd $SRC_DIR/singer-connectors/$1
     make_virtualenv $1
 }
@@ -47,6 +86,15 @@ install_cli() {
     cd $SRC_DIR/cli
     make_virtualenv cli
 }
+
+# Parse command line arguments
+for arg in "$@"; do
+    case $1 in
+        # Auto accept license agreemnets. Useful if PipelineWise installed by an automated script
+        --acceptlicenses)
+            ACCEPT_LICENSES="YES"
+    esac
+done
 
 
 # Install Singer connectors
