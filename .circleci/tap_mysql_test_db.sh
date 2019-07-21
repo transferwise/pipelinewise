1bfb851e8d5ba9ec5c6439d774705da65004c096
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-07-21 21:49:47 +0100

    WIP - circleci integration test

---
+++ .circleci/tap_mysql_test_db.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+#
+# Building a test MySQL database for integration testing of tap-mysql 
+# The sample database available at https://github.com/ikostan/RESTAURANT-DATABASE
+
+
+# To run this script some environment variables must be set.
+# Normally it's defined in .circleci/config.yml
+if [[ -z "${TAP_MYSQL_HOST}" || -z "${TAP_MYSQL_PORT}" || -z "${TAP_MYSQL_USER}" || -z "${TAP_MYSQL_PASSWORD}" || -z "${TAP_MYSQL_DBNAME}" ]]; then
+    echo "ERROR: One or more required environment variable is not defined:"
+    echo "       - TAP_MYSQL_HOST"
+    echo "       - TAP_MYSQL_PORT"
+    echo "       - TAP_MYSQL_USER"
+    echo "       - TAP_MYSQL_PASSWORD"
+    echo "       - TAP_MYSQL_DBNAME"
+    exit 1
+fi
+
+# Pass environment variables to MySQL compatible ones
+MYSQL_HOST=${TAP_MYSQL_HOST}
+MYSQL_TCP_PORT=${TAP_MYSQL_PORT}
+MYSQL_PWD=${TAP_MYSQL_PASSWORD} 
+
+# Download the sample database and build it
+wget https://raw.githubusercontent.com/ikostan/RESTAURANT-DATABASE/master/DB_backup/structure_and_data/grp24.sql
+mysql --user ${TAP_MYSQL_USER} ${TAP_MYSQL_DBNAME} < grp24.sql
--- .circleci/tap_postgres_test_db.sh
+++ .circleci/tap_postgres_test_db.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -e
 #
 # Building a test PostgreSQL database for integration testing of tap-postgres 
-# The database is a sample database availabel at https://github.com/morenoh149/postgresDBSamples
-#
-# The following envrionment variables must set in circleci config.yml
-#   TAP_POSTGRES_HOST: localhost
-#   TAP_POSTGRES_PORT: 5432
-#   TAP_POSTGRES_USER: pipelinewise
-#   TAP_POSTGRES_PASSWORD: secret
-#   TAP_POSTGRES_DBNAME: postgres_source_db
+# The sample database available at https://github.com/morenoh149/postgresDBSamples
 
+
+# To run this script some environment variables must be set.
+# Normally it's defined in .circleci/config.yml
+if [[ -z "${TAP_POSTGRES_HOST}" || -z "${TAP_POSTGRES_PORT}" || -z "${TAP_POSTGRES_USER}" || -z "${TAP_POSTGRES_PASSWORD}" || -z "${TAP_POSTGRES_DBNAME}" ]]; then
+    echo "ERROR: One or more required environment variable is not defined:"
+    echo "       - TAP_POSTGRES_HOST"
+    echo "       - TAP_POSTGRES_PORT"
+    echo "       - TAP_POSTGRES_USER"
+    echo "       - TAP_POSTGRES_PASSWORD"
+    echo "       - TAP_POSTGRES_DBNAME"
+    exit 1
+fi
 
 # Create a postgres password file for non-interaction connection
 PGPASSFILE=~/.pgpass
 echo ${TAP_POSTGRES_HOST}:${TAP_POSTGRES_PORT}:${TAP_POSTGRES_DBNAME}:${TAP_POSTGRES_USER}:${TAP_POSTGRES_PASSWORD} > ${PGPASSFILE}
 chmod 0600 ${PGPASSFILE}
-
-DB_CONNECTION=`cat ${PGPASSFILE}`
-echo "Postgres test DB connection: ${DB_CONNECTION}"
 
 # Download the sample database and build it
 wget https://raw.githubusercontent.com/morenoh149/postgresDBSamples/master/chinook-1.4/Chinook_PostgreSql_utf8.sql
