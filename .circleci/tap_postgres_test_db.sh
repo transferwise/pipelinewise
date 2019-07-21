c59d778513ef3408185a72c15cb80a701c6f77fa
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-07-21 21:06:19 +0100

    WIP - circleci integration test

--- .circleci/tap_postgres_test_db.sh
+++ .circleci/tap_postgres_test_db.sh
@@ -2,10 +2,16 @@
 #
 # Building a test PostgreSQL database for integration testing of tap-postgres 
 # The database is a sample database availabel at https://github.com/morenoh149/postgresDBSamples
+#
+# The following envrionment variables must set in circleci config.yml
+#   TAP_POSTGRES_HOST: localhost
+#   TAP_POSTGRES_PORT: 5432
+#   TAP_POSTGRES_USER: pipelinewise
+#   TAP_POSTGRES_PASSWORD: secret
+#   TAP_POSTGRES_DBNAME: postgres_source_db
 
 
 # Create a postgres password file for non-interaction connection
-# Envrionment variables are set in circleci config.yml
 PGPASSFILE=~/.pgpass
 echo ${TAP_POSTGRES_HOST}:${TAP_POSTGRES_PORT}:${TAP_POSTGRES_DBNAME}:${TAP_POSTGRES_USER}:${TAP_POSTGRES_PASSWORD} > ${PGPASSFILE}
 chmod 0600 ${PGPASSFILE}
@@ -15,4 +21,4 @@ echo "Postgres test DB connection: ${DB_CONNECTION}"
 
 # Download the sample database and build it
 wget https://raw.githubusercontent.com/morenoh149/postgresDBSamples/master/chinook-1.4/Chinook_PostgreSql_utf8.sql
-psql -f Chinook_PostgreSql_utf8.sql -d postgres_source_db
+psql -U ${TAP_POSTGRES_USER} -h ${TAP_POSTGRES_HOST} -f Chinook_PostgreSql_utf8.sql -d ${TAP_POSTGRES_DBNAME}
