6b4c6957763b66b1e760f91241f60d7578df8be4
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-09-04 00:45:59 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ def setup(app):
 project = 'PipelineWise'
 copyright = '2019, TransferWise Ltd.'
 author = 'Peter Kosztolanyi'
-version = '0.9.1'
+version = '0.9.2'
 
 
 # -- General configuration ---------------------------------------------------
