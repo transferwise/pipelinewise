149d8eba751971b6244cc969cb857092cc44a8df
Louis Pieterse <louis.pieterse@transferwise.com>
2019-09-11 15:00:46 +0100

    add more test data to db_postgres_source

--- tests/db/tap_postgres_db.sh
+++ tests/db/tap_postgres_db.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 #
 # Building a test PostgreSQL database for integration testing of tap-postgres 
-# The sample database available at https://github.com/morenoh149/postgresDBSamples
 
-TEST_DB_URL=https://raw.githubusercontent.com/morenoh149/postgresDBSamples/master/worldDB-1.0/world.sql
-TEST_DB_TMP_SQL=/tmp/test-postgres-db.sql
+
+TEST_DB_SQL=./tests/db/tap_postgres_data.sql
 echo "Building test PostgreSQL database from ${TEST_DB_URL}..."
 
 # To run this script some environment variables must be set.
@@ -24,14 +23,11 @@ PGPASSFILE=~/.pgpass
 echo ${DB_TAP_POSTGRES_HOST}:${DB_TAP_POSTGRES_PORT}:${DB_TAP_POSTGRES_DB}:${DB_TAP_POSTGRES_USER}:${DB_TAP_POSTGRES_PASSWORD} > ${PGPASSFILE}
 chmod 0600 ${PGPASSFILE}
 
-# Download the sample database and build it
-wget -O ${TEST_DB_TMP_SQL} ${TEST_DB_URL}
-
 # Drop target tables if exists, the test db sql works only if tables not exist
+psql -U ${DB_TAP_POSTGRES_USER} -h ${DB_TAP_POSTGRES_HOST} -c 'DROP TABLE IF EXISTS public.edgyData CASCADE;' ${DB_TAP_POSTGRES_DB}
 psql -U ${DB_TAP_POSTGRES_USER} -h ${DB_TAP_POSTGRES_HOST} -c 'DROP TABLE IF EXISTS public.countrylanguage CASCADE;' ${DB_TAP_POSTGRES_DB}
 psql -U ${DB_TAP_POSTGRES_USER} -h ${DB_TAP_POSTGRES_HOST} -c 'DROP TABLE IF EXISTS public.country CASCADE;' ${DB_TAP_POSTGRES_DB}
 psql -U ${DB_TAP_POSTGRES_USER} -h ${DB_TAP_POSTGRES_HOST} -c 'DROP TABLE IF EXISTS public.city CASCADE;' ${DB_TAP_POSTGRES_DB}
 
 # Build the test DB
-psql -U ${DB_TAP_POSTGRES_USER} -h ${DB_TAP_POSTGRES_HOST} -f ${TEST_DB_TMP_SQL} -d ${DB_TAP_POSTGRES_DB}
-rm -f ${TEST_DB_TMP_SQL}
+psql -U ${DB_TAP_POSTGRES_USER} -h ${DB_TAP_POSTGRES_HOST} -f ${TEST_DB_SQL} -d ${DB_TAP_POSTGRES_DB}
