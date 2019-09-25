163a1f5e1b2a152f0535397c2bdebe455ed732da
Louis Pieterse <louis.pieterse@transferwise.com>
2019-09-25 11:32:47 +0300

    spice up logical stream

--- dev-project/db/tap_postgres_logical_data.sql
+++ dev-project/db/tap_postgres_logical_data.sql
@@ -1,13 +1,10 @@
--- ====================================
--- Create fresh replication slot
--- ====================================
+-- create fresh replication slot
 SELECT pg_drop_replication_slot('pipelinewise_postgres_source_db');
 SELECT slot_name, lsn, lsn - '0/0'::pg_lsn as lsn FROM pg_create_logical_replication_slot('pipelinewise_postgres_source_db', 'wal2json');
 
--- ====================================
--- Schema logical1
--- ====================================
+-- create structures
 DROP SCHEMA IF EXISTS logical1 CASCADE;
+
 CREATE SCHEMA logical1;
 
 CREATE TABLE logical1.logical1_table1(
@@ -16,13 +13,9 @@ CREATE TABLE logical1.logical1_table1(
     PRIMARY KEY (cid)
 );
 
-INSERT INTO logical1.logical1_table1 (cvarchar) VALUES ('one please');
+DROP SCHEMA IF EXISTS logical2 CASCADE;
 
-ALTER TABLE logical1.logical1_table1
-ADD COLUMN cvarchar2 varchar
-;
-
-INSERT INTO logical1.logical1_table1 (cvarchar, cvarchar2) VALUES ('two please', 'two please');
+CREATE SCHEMA logical2;
 
 CREATE TABLE logical1.logical1_table2(
     cid serial NOT NULL,
@@ -30,46 +23,14 @@ CREATE TABLE logical1.logical1_table2(
     PRIMARY KEY (cid)
 );
 
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('one please');
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('two please');
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('three please');
-
-BEGIN;
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('four please');
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('five please');
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('six please');
-COMMIT;
-
-INSERT INTO logical1.logical1_table2 (cvarchar) VALUES
-    ('seven please'),
-    ('eight please'),
-    ('nine please')
-;
-
-DELETE FROM logical1.logical1_table2 WHERE cvarchar='four please';
-
--- ====================================
--- Schema logical2
--- ====================================
-DROP SCHEMA IF EXISTS logical2 CASCADE;
-CREATE SCHEMA logical2;
-
 CREATE TABLE logical2.logical2_table1(
     cid serial NOT NULL,
     cvarchar varchar,
     PRIMARY KEY (cid)
 );
 
-INSERT INTO logical2.logical2_table1 (cvarchar) VALUES ('one please');
-INSERT INTO logical2.logical2_table1 (cvarchar) VALUES ('two please');
-INSERT INTO logical2.logical2_table1 (cvarchar) VALUES ('three please');
-
-UPDATE logical2.logical2_table1 SET cvarchar = 'I meant eight, please';
-
--- ====================================
--- Schema logical3
--- ====================================
 DROP SCHEMA IF EXISTS logical3 CASCADE;
+
 CREATE SCHEMA logical3;
 
 CREATE TABLE logical3.logical3_table1(
@@ -78,10 +39,68 @@ CREATE TABLE logical3.logical3_table1(
     PRIMARY KEY (cid)
 );
 
-INSERT INTO logical3.logical3_table1 (cvarchar) VALUES ('one please');
-INSERT INTO logical3.logical3_table1 (cvarchar) VALUES ('two please');
-INSERT INTO logical3.logical3_table1 (cvarchar) VALUES ('three please');
+-- activity stream
+INSERT INTO logical1.logical1_table1 (cvarchar) VALUES ('inserted row');
 
-UPDATE logical3.logical3_table1 SET cvarchar = 'I meant eight, please';
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+BEGIN;
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('delete later');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('delete later');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+COMMIT;
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+ALTER TABLE logical1.logical1_table1 ADD COLUMN cvarchar2 varchar ;
+
+INSERT INTO logical1.logical1_table1 (cvarchar, cvarchar2) VALUES ('inserted row', 'inserted row');
+
+INSERT INTO logical1.logical1_table1 (cvarchar, cvarchar2) VALUES ('inserted row', 'inserted row');
+
+INSERT INTO logical1.logical1_table1 (cvarchar, cvarchar2) VALUES ('inserted row', 'inserted row');
+
+DELETE FROM logical1.logical1_table2 WHERE cvarchar = 'delete later';
+
+UPDATE logical1.logical1_table2 SET cvarchar = 'updated row';
+
+BEGIN;
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+    INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+COMMIT;
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES ('inserted row');
+
+INSERT INTO logical1.logical1_table2 (cvarchar) VALUES
+    ('inserted row'),
+    ('inserted row'),
+    ('inserted row')
+;
+
+DELETE FROM logical1.logical1_table2 WHERE cvarchar='inserted row';
+
+INSERT INTO logical2.logical2_table1 (cvarchar) VALUES ('inserted row');
+INSERT INTO logical2.logical2_table1 (cvarchar) VALUES ('inserted row');
+INSERT INTO logical2.logical2_table1 (cvarchar) VALUES ('inserted row');
+
+UPDATE logical2.logical2_table1 SET cvarchar = 'updated row';
+
+INSERT INTO logical3.logical3_table1 (cvarchar) VALUES ('inserted row');
+INSERT INTO logical3.logical3_table1 (cvarchar) VALUES ('inserted row');
+INSERT INTO logical3.logical3_table1 (cvarchar) VALUES ('inserted row');
 
 DROP SCHEMA IF EXISTS logical3 CASCADE;
