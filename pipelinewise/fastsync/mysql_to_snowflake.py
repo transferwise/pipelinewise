f79f8aa628d86060e6a53312d05cdd56a98eea7c
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-10-29 19:52:14 +0000

    mysql-to-snowflake fastsync: refresh information_schema columns cache

--- pipelinewise/fastsync/mysql_to_snowflake.py
+++ pipelinewise/fastsync/mysql_to_snowflake.py
@@ -155,8 +155,8 @@ def main_impl():
         table_sync_excs = list(filter(None, p.map(sync_table, args.tables)))
 
     # Refresh information_schema columns cache
-    #snowflake = FastSyncTargetSnowflake(args.target, args.transform)
-    #snowflake.cache_information_schema_columns(args.tables)
+    snowflake = FastSyncTargetSnowflake(args.target, args.transform)
+    snowflake.cache_information_schema_columns(args.tables)
 
     # Log summary
     end_time = datetime.now()
