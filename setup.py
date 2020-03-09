24cbacdff16d78ffc0d3ea468762fc50fcbbcd59
Samira-El <54845154+Samira-El@users.noreply.github.com>
2020-03-09 15:53:23 +0200

    Bump patch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='pipelinewise',
-      version='0.13.2',
+      version='0.13.3',
       description='PipelineWise',
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
