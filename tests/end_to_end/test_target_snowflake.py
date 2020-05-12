b486574a22aa217dc2841b33f0bd56a032451de2
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2020-05-12 10:14:26 +0100

    [AP-692] Upload only if tap_s3_csv credentials configured

--- tests/end_to_end/test_target_snowflake.py
+++ tests/end_to_end/test_target_snowflake.py
@@ -44,7 +44,8 @@ class TestTargetSnowflake:
         # Setup and clean source and target databases
         self.e2e.setup_tap_mysql()
         self.e2e.setup_tap_postgres()
-        self.e2e.setup_tap_s3_csv()
+        if self.e2e.env['TAP_S3_CSV']['is_configured']:
+            self.e2e.setup_tap_s3_csv()
         self.e2e.setup_target_snowflake()
 
         # Import project
