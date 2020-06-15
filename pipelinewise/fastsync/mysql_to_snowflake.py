8694f6a96d3590ed6ac36c6431bfff40805eca99
mlavoie-sm360 <mlavoie@solutionsmedias360.com>
2020-06-15 08:21:31 +0100

    Update mysql_to_snowflake.py

--- pipelinewise/fastsync/mysql_to_snowflake.py
+++ pipelinewise/fastsync/mysql_to_snowflake.py
@@ -3,15 +3,14 @@ import logging
 import os
 import sys
 import time
-import multiprocessing
-
-from typing import Union
-from argparse import Namespace
 from functools import partial
-from datetime import datetime
+from argparse import Namespace
+import multiprocessing
+from typing import Union
 
+from datetime import datetime
 from .commons import utils
-from .commons.tap_mongodb import FastSyncTapMongoDB
+from .commons.tap_mysql import FastSyncTapMySql
 from .commons.target_snowflake import FastSyncTargetSnowflake
 
 LOGGER = logging.getLogger(__name__)
@@ -21,9 +20,7 @@ REQUIRED_CONFIG_KEYS = {
         'host',
         'port',
         'user',
-        'password',
-        'auth_database',
-        'dbname',
+        'password'
     ],
     'target': [
         'account',
@@ -40,54 +37,74 @@ REQUIRED_CONFIG_KEYS = {
 LOCK = multiprocessing.Lock()
 
 
-def tap_type_to_target_type(mongo_type):
-    """Data type mapping from MongoDB to Snowflake"""
+def tap_type_to_target_type(mysql_type, mysql_column_type):
+    """Data type mapping from MySQL to Snowflake"""
     return {
-        'string': 'TEXT',
-        'object': 'VARIANT',
-        'array': 'VARIANT',
+        'char': 'VARCHAR',
+        'varchar': 'VARCHAR',
+        'binary': 'BINARY',
+        'varbinary': 'BINARY',
+        'blob': 'VARCHAR',
+        'tinyblob': 'VARCHAR',
+        'mediumblob': 'VARCHAR',
+        'longblob': 'VARCHAR',
+        'geometry': 'VARCHAR',
+        'text': 'VARCHAR',
+        'tinytext': 'VARCHAR',
+        'mediumtext': 'VARCHAR',
+        'longtext': 'VARCHAR',
+        'enum': 'VARCHAR',
+        'int': 'NUMBER',
+        'tinyint': 'BOOLEAN' if mysql_column_type == 'tinyint(1)' else 'NUMBER',
+        'smallint': 'NUMBER',
+        'bigint': 'NUMBER',
+        'bit': 'BOOLEAN',
+        'decimal': 'FLOAT',
+        'double': 'FLOAT',
+        'float': 'FLOAT',
+        'bool': 'BOOLEAN',
+        'boolean': 'BOOLEAN',
         'date': 'TIMESTAMP_NTZ',
         'datetime': 'TIMESTAMP_NTZ',
         'timestamp': 'TIMESTAMP_NTZ',
-    }.get(mongo_type, 'VARCHAR')
+        'json': 'VARIANT'
+    }.get(mysql_type, 'VARCHAR')
 
 
-# pylint: disable=too-many-locals
 def sync_table(table: str, args: Namespace) -> Union[bool, str]:
     """Sync one table"""
-    mongodb = FastSyncTapMongoDB(args.tap, tap_type_to_target_type)
+    mysql = FastSyncTapMySql(args.tap, tap_type_to_target_type)
     snowflake = FastSyncTargetSnowflake(args.target, args.transform)
 
     try:
-        dbname = args.tap.get('dbname')
-        filename = 'pipelinewise_fastsync_{}_{}_{}.csv.gz'.format(dbname, table, time.strftime('%Y%m%d-%H%M%S'))
+        filename = 'pipelinewise_fastsync_{}_{}.csv.gz'.format(table, time.strftime('%Y%m%d-%H%M%S'))
         filepath = os.path.join(args.temp_dir, filename)
         target_schema = utils.get_target_schema(args.target, table)
 
-        # Open connection
-        mongodb.open_connection()
+        # Open connection and get binlog file position
+        mysql.open_connections()
 
-        # Get bookmark - LSN position or Incremental Key value
-        bookmark = utils.get_bookmark_for_table(table, args.properties, mongodb, dbname=dbname)
+        # Get bookmark - Binlog position or Incremental Key value
+        bookmark = utils.get_bookmark_for_table(table, args.properties, mysql)
 
         # Exporting table data, get table definitions and close connection to avoid timeouts
-        mongodb.copy_table(table, filepath, args.temp_dir)
+        mysql.copy_table(table, filepath)
         size_bytes = os.path.getsize(filepath)
-        snowflake_types = mongodb.map_column_types_to_target()
+        snowflake_types = mysql.map_column_types_to_target(table)
         snowflake_columns = snowflake_types.get('columns', [])
-        primary_key = snowflake_types['primary_key']
-        mongodb.close_connection()
+        primary_key = snowflake_types.get('primary_key')
+        mysql.close_connections()
 
         # Uploading to S3
         s3_key = snowflake.upload_to_s3(filepath, table, tmp_dir=args.temp_dir)
-        # os.remove(filepath)
+        os.remove(filepath)
 
         # Creating temp table in Snowflake
         snowflake.create_schema(target_schema)
         snowflake.create_table(target_schema, table, snowflake_columns, primary_key, is_temporary=True)
 
         # Load into Snowflake table
-        snowflake.copy_to_table(s3_key, target_schema, table, size_bytes, is_temporary=True, skip_csv_header=True)
+        snowflake.copy_to_table(s3_key, target_schema, table, size_bytes, is_temporary=True)
 
         # Obfuscate columns
         snowflake.obfuscate_columns(target_schema, table)
