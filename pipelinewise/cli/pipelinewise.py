6be4ee8e4ea8096deb955f823cf6ca81476135e1
Henri Blancke <blanckehenri@gmail.com>
2020-09-16 10:07:04 -0400

    [FIX] Error "stream_table_mdata_idx accessed before assignment" (#510)
    * [ADD] empty breadcrumb if none
    
    * [UPD] raise when no empty breadcrumb

--- pipelinewise/cli/pipelinewise.py
+++ pipelinewise/cli/pipelinewise.py
@@ -557,7 +557,7 @@ class PipelineWise:
                 try:
                     stream_table_mdata_idx = [i for i, md in enumerate(stream['metadata']) if md['breadcrumb'] == []][0]
                 except Exception:
-                    pass
+                    raise Exception(f'Metadata of stream {tap_stream_id} doesn\'t have an empty breadcrumb')
 
                 if tap_stream_sel:
                     self.logger.debug('Mark %s tap_stream_id as selected with properties %s', tap_stream_id,
