b34cf837951c2f16974f72b107523ceca4c6ae02
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2020-10-01 10:29:22 +0100

    [RELEASE] v0.24.0 (#544)

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ def setup(app):
 project = 'PipelineWise'
 copyright = '2020, TransferWise Ltd.'
 author = 'TransferWise'
-version = '0.23.0'
+version = '0.24.0'
 
 
 # -- General configuration ---------------------------------------------------
