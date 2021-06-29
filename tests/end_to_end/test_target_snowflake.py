a0600baa1a25b6f5a26ec88786f9eb7d2fbe31b6
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2021-06-29 10:36:31 +0100

    Run snowflake e2e test only when it's enabled (#732)

--- tests/end_to_end/test_target_snowflake.py
+++ tests/end_to_end/test_target_snowflake.py
@@ -147,6 +147,7 @@ class TestTargetSnowflake:
                                             mysql_to_snowflake.tap_type_to_target_type)
 
     # pylint: disable=invalid-name
+    @pytest.mark.dependency(depends=['import_config'])
     def test_resync_pg_to_sf_with_split_large_files(self, tap_postgres_id=TAP_POSTGRES_SPLIT_LARGE_FILES_ID):
         """Resync tables from Postgres to Snowflake using splitting large files option"""
         assertions.assert_resync_tables_success(tap_postgres_id, TARGET_ID, profiling=True)
