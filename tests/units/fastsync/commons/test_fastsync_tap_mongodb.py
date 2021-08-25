fe7d83ba1531d68fbfd2b9cddd7fd8819a5e6278
Samira <samira.elaabidi@transferwise.com>
2021-08-25 16:33:08 +0300

    update unit tests

--- tests/units/fastsync/commons/test_fastsync_tap_mongodb.py
+++ tests/units/fastsync/commons/test_fastsync_tap_mongodb.py
@@ -1,10 +1,14 @@
+import datetime
+import decimal
+import re
+import string
 import time
 import os
-from unittest import TestCase
-from unittest.mock import patch, Mock
+import random
+import bson
 
-from bson import ObjectId, Timestamp
-from mock import PropertyMock, call
+from unittest import TestCase
+from unittest.mock import patch, Mock, PropertyMock, call
 from pymongo import MongoClient
 from pymongo.change_stream import DatabaseChangeStream
 from pymongo.database import Database
@@ -13,6 +17,47 @@ from pipelinewise.fastsync.commons.errors import TableNotFoundError, ExportError
 from pipelinewise.fastsync.commons.tap_mongodb import FastSyncTapMongoDB
 
 TEST_EXPORT_FILE = 'file.csv.gzip'
+
+
+def random_string_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def generate_all_datatypes_doc():
+    pattern = re.compile('.*')
+    regex = bson.Regex.from_native(pattern)
+    regex.flags ^= re.UNICODE
+
+    return {
+        "_id": bson.ObjectId(),
+        "double_field": random.randrange(-15, 15) / 3,
+        "string_field": random_string_generator(100),
+        "object_field": {
+            "obj_field_1_key": "obj_field_1_val",
+            "obj_field_2_key": "obj_field_2_val"
+        },
+        "array_field": [
+            None,
+            random.randrange(-1, 1) / 5,
+            {"k": "v"},
+            "array_item",
+            bson.Decimal128(decimal.Decimal(f'{random.randrange(-10, 10) / 6}')),
+        ],
+        "binary_data_field": b"a binary string",
+        "object_id_field": bson.objectid.ObjectId(),
+        "boolean_field": True,
+        "date_field": datetime.datetime.now(),
+        "null_field": None,
+        "regex_field": regex,
+        "32_bit_integer_field": 32,
+        "timestamp_field": bson.timestamp.Timestamp(int(time.time()), random.randint(0, 100)),
+        "64_bit_integer_field": 34359738368,
+        "decimal_field": bson.Decimal128(decimal.Decimal(f'{random.randrange(-100, 100) / 33}')),
+        "javaScript_field": bson.code.Code("var x, y, z;"),
+        "javaScript_with_scope_field": bson.code.Code("function incrementX() { x++; }", scope={"x": 1}),
+        "min_key_field": bson.min_key.MinKey(),
+        "max_key_field": bson.max_key.MaxKey()
+    }
 
 
 # pylint: disable=invalid-name,no-self-use
@@ -129,11 +174,8 @@ class TestFastSyncTapMongoDB(TestCase):
                     with patch('pipelinewise.fastsync.commons.tap_mongodb.bson.decode_file_iter') as \
                             bson_decode_iter_mock:
 
-                        bson_decode_iter_mock.return_value = [
-                            {'_id': ObjectId('0123456789ab0123456789aa'), 'key1': 1, 'key2': time.time()},
-                            {'_id': ObjectId('0123456789ab0123456789ab'), 'key1': 2},
-                            {'_id': ObjectId('0123456789ab0123456789ac'), 'key3': Timestamp(10000, 50)},
-                        ]
+                        # generate 10 documents
+                        bson_decode_iter_mock.return_value = (generate_all_datatypes_doc() for _ in range(10))
 
                         mock_enter.return_value.open.return_value = Mock()
 
