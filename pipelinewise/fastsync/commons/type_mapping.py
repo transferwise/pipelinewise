6024bc02c57cb72f5bf522eec5ebc1c832acc861
samira.elaabidi <samira.elaabidi@transferwise.com>
2021-09-14 15:20:01 +0300

    rename constants and use elif

--- pipelinewise/fastsync/commons/type_mapping.py
+++ pipelinewise/fastsync/commons/type_mapping.py
@@ -6,33 +6,33 @@ from typing import Optional
 # Please keep this list up to date with Singer taps
 # https://github.com/transferwise/pipelinewise-tap-mysql/blob/master/tap_mysql/discover_utils.py
 
-__STRING_TYPES = {'char', 'varchar', 'text', 'tinytext', 'mediumtext', 'longtext', 'enum'}
+__MYSQL_STRING_TYPES = {'char', 'varchar', 'text', 'tinytext', 'mediumtext', 'longtext', 'enum'}
 
-__BINARY_TYPES = {'binary', 'varbinary'}
+__MYSQL_BINARY_TYPES = {'binary', 'varbinary'}
 
-__SPATIAL_TYPES = {
+__MYSQL_SPATIAL_TYPES = {
     'geometry', 'point', 'linestring', 'polygon', 'multipoint',
     'multilinestring', 'multipolygon', 'geometrycollection'
 }
 
-__FLOAT_TYPES = {'float', 'double', 'decimal'}
+__MYSQL_FLOAT_TYPES = {'float', 'double', 'decimal'}
 
-__JSON_TYPES = {'json'}
+__MYSQL_JSON_TYPES = {'json'}
 
-__BOOL_TYPES = {'bit', 'boolean', 'bool'}
+__MYSQL_BOOL_TYPES = {'bit', 'boolean', 'bool'}
 
-__DATETIME_TYPES = {'datetime', 'timestamp', 'time', 'date'}
+__MYSQL_DATETIME_TYPES = {'datetime', 'timestamp', 'time', 'date'}
 
-__INTEGER_TYPES = {'tinyint', 'int', 'smallint', 'mediumint', 'bigint'}
+__MYSQL_INTEGER_TYPES = {'tinyint', 'int', 'smallint', 'mediumint', 'bigint'}
 
-SUPPORTED_MYSQL_DATATYPES = (__STRING_TYPES
-                             .union(__FLOAT_TYPES)
-                             .union(__DATETIME_TYPES)
-                             .union(__BINARY_TYPES)
-                             .union(__SPATIAL_TYPES)
-                             .union(__BOOL_TYPES)
-                             .union(__JSON_TYPES)
-                             .union(__INTEGER_TYPES))
+SUPPORTED_MYSQL_DATATYPES = (__MYSQL_STRING_TYPES
+                             .union(__MYSQL_FLOAT_TYPES)
+                             .union(__MYSQL_DATETIME_TYPES)
+                             .union(__MYSQL_BINARY_TYPES)
+                             .union(__MYSQL_SPATIAL_TYPES)
+                             .union(__MYSQL_BOOL_TYPES)
+                             .union(__MYSQL_JSON_TYPES)
+                             .union(__MYSQL_INTEGER_TYPES))
 
 
 REDSHIFT_DEFAULT_VARCHAR_LENGTH = 10000
@@ -51,31 +51,31 @@ def __mysql_to_sf_mapper(mysql_column_datatype: str, mysql_column_type: str) -> 
     """
     target_type = None
 
-    if mysql_column_datatype in __STRING_TYPES:
+    if mysql_column_datatype in __MYSQL_STRING_TYPES:
         target_type = 'VARCHAR'
 
-    if mysql_column_datatype in __BINARY_TYPES:
+    elif mysql_column_datatype in __MYSQL_BINARY_TYPES:
         target_type = 'BINARY'
 
-    if mysql_column_datatype in __INTEGER_TYPES:
+    elif mysql_column_datatype in __MYSQL_INTEGER_TYPES:
         target_type = 'NUMBER'
 
         if mysql_column_datatype == 'tinyint' and mysql_column_type and mysql_column_type.startswith('tinyint(1)'):
             target_type = 'BOOLEAN'
 
-    if mysql_column_datatype in __BOOL_TYPES:
+    elif mysql_column_datatype in __MYSQL_BOOL_TYPES:
         target_type = 'BOOLEAN'
 
-    if mysql_column_datatype in __FLOAT_TYPES:
+    elif mysql_column_datatype in __MYSQL_FLOAT_TYPES:
         target_type = 'FLOAT'
 
-    if mysql_column_datatype in __DATETIME_TYPES:
+    elif mysql_column_datatype in __MYSQL_DATETIME_TYPES:
         target_type = 'TIMESTAMP_NTZ'
 
         if mysql_column_datatype == 'time':
             target_type = 'TIME'
 
-    if mysql_column_datatype in __JSON_TYPES or mysql_column_datatype in __SPATIAL_TYPES:
+    elif mysql_column_datatype in __MYSQL_JSON_TYPES or mysql_column_datatype in __MYSQL_SPATIAL_TYPES:
         target_type = 'VARIANT'
 
     return target_type
@@ -95,19 +95,19 @@ def __mysql_to_postgres_mapper(mysql_column_datatype: str, mysql_column_type: st
     """
     return_type = None
 
-    if mysql_column_datatype in __STRING_TYPES or mysql_column_datatype in __BINARY_TYPES:
+    if mysql_column_datatype in __MYSQL_STRING_TYPES or mysql_column_datatype in __MYSQL_BINARY_TYPES:
         return_type = 'CHARACTER VARYING'
 
-    if mysql_column_datatype in __JSON_TYPES or mysql_column_datatype in __SPATIAL_TYPES:
+    elif mysql_column_datatype in __MYSQL_JSON_TYPES or mysql_column_datatype in __MYSQL_SPATIAL_TYPES:
         return_type = 'JSONB'
 
-    if mysql_column_datatype in __BOOL_TYPES:
+    elif mysql_column_datatype in __MYSQL_BOOL_TYPES:
         return_type = 'BOOLEAN'
 
-    if mysql_column_datatype in __FLOAT_TYPES:
+    elif mysql_column_datatype in __MYSQL_FLOAT_TYPES:
         return_type = 'DOUBLE PRECISION'
 
-    if mysql_column_datatype in __INTEGER_TYPES:
+    elif mysql_column_datatype in __MYSQL_INTEGER_TYPES:
 
         if mysql_column_datatype == 'tinyint':
             return_type = 'BOOLEAN' if mysql_column_type and \
@@ -125,7 +125,7 @@ def __mysql_to_postgres_mapper(mysql_column_datatype: str, mysql_column_type: st
         else:
             return_type = 'NUMERIC'
 
-    if mysql_column_datatype in __DATETIME_TYPES:
+    elif mysql_column_datatype in __MYSQL_DATETIME_TYPES:
         return_type = 'TIME WITHOUT TIME ZONE' if mysql_column_datatype == 'time' else 'TIMESTAMP WITHOUT TIME ZONE'
 
     return return_type
@@ -145,25 +145,25 @@ def __mysql_to_bigquery_mapper(mysql_column_datatype: str, mysql_column_type: st
     """
     return_type = None
 
-    if mysql_column_datatype in __STRING_TYPES or \
-            mysql_column_datatype in __BINARY_TYPES \
-            or mysql_column_datatype in __SPATIAL_TYPES or \
-            mysql_column_datatype in __JSON_TYPES:
+    if mysql_column_datatype in __MYSQL_STRING_TYPES or \
+            mysql_column_datatype in __MYSQL_BINARY_TYPES \
+            or mysql_column_datatype in __MYSQL_SPATIAL_TYPES or \
+            mysql_column_datatype in __MYSQL_JSON_TYPES:
         return_type = 'STRING'
 
-    if mysql_column_datatype in __BOOL_TYPES:
+    elif mysql_column_datatype in __MYSQL_BOOL_TYPES:
         return_type = 'BOOL'
 
-    if mysql_column_datatype in __FLOAT_TYPES:
+    elif mysql_column_datatype in __MYSQL_FLOAT_TYPES:
         return_type = 'NUMERIC'
 
-    if mysql_column_datatype in __INTEGER_TYPES:
+    elif mysql_column_datatype in __MYSQL_INTEGER_TYPES:
         return_type = 'INT64'
 
         if mysql_column_datatype == 'tinyint' and mysql_column_type and mysql_column_type.startswith('tinyint(1)'):
             return_type = 'BOOLEAN'
 
-    if mysql_column_datatype in __DATETIME_TYPES:
+    elif mysql_column_datatype in __MYSQL_DATETIME_TYPES:
         if mysql_column_datatype == 'time':
             return_type = 'TIME'
         else:
@@ -187,7 +187,7 @@ def __mysql_to_redshift_mapper(mysql_column_datatype: str, mysql_column_type: st
 
     return_type = None
 
-    if mysql_column_datatype in __STRING_TYPES:
+    if mysql_column_datatype in __MYSQL_STRING_TYPES:
 
         return_type = f'CHARACTER VARYING({REDSHIFT_LONG_VARCHAR_LENGTH})'
 
@@ -197,25 +197,25 @@ def __mysql_to_redshift_mapper(mysql_column_datatype: str, mysql_column_type: st
         elif mysql_column_datatype == 'enum':
             return_type = f'CHARACTER VARYING({REDSHIFT_DEFAULT_VARCHAR_LENGTH})'
 
-    if mysql_column_datatype in __BINARY_TYPES or mysql_column_datatype in __JSON_TYPES:
+    elif mysql_column_datatype in __MYSQL_BINARY_TYPES or mysql_column_datatype in __MYSQL_JSON_TYPES:
         return_type = f'CHARACTER VARYING({REDSHIFT_LONG_VARCHAR_LENGTH})'
 
-    if mysql_column_datatype in __SPATIAL_TYPES:
+    elif mysql_column_datatype in __MYSQL_SPATIAL_TYPES:
         return_type = f'CHARACTER VARYING({REDSHIFT_DEFAULT_VARCHAR_LENGTH})'
 
-    if mysql_column_datatype in __BOOL_TYPES:
+    elif mysql_column_datatype in __MYSQL_BOOL_TYPES:
         return_type = 'BOOLEAN'
 
-    if mysql_column_datatype in __FLOAT_TYPES:
+    elif mysql_column_datatype in __MYSQL_FLOAT_TYPES:
         return_type = 'FLOAT'
 
-    if mysql_column_datatype in __INTEGER_TYPES:
+    elif mysql_column_datatype in __MYSQL_INTEGER_TYPES:
 
         return_type = 'NUMERIC NULL'
         if mysql_column_datatype == 'tinyint' and mysql_column_type and mysql_column_type.startswith('tinyint(1)'):
             return_type = 'BOOLEAN'
 
-    if mysql_column_datatype in __DATETIME_TYPES:
+    elif mysql_column_datatype in __MYSQL_DATETIME_TYPES:
         if mysql_column_datatype == 'time':
             return_type = 'TIME WITHOUT TIME ZONE'
         else:
