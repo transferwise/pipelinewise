f7d6b2e6fa52be2cef948ec609dedf5682812874
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2022-01-25 14:33:22 +0000

    Bump minor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='pipelinewise',
-      version='0.38.1',
+      version='0.39.0',
       description='PipelineWise',
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
