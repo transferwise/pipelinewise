bfd4f2894a6696ddd0bda91278be1d071309b869
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-02-18 14:40:45 +0000

    Allow `FastSyncTargetBigquery` to upload to GCS in parallel

--- pipelinewise/fastsync/postgres_to_bigquery.py
+++ pipelinewise/fastsync/postgres_to_bigquery.py
@@ -106,9 +106,8 @@ def sync_table(table: str, args: Namespace) -> Union[bool, str]:
         postgres.close_connection()
 
         # Uploading to GCS
-        gcs_blobs = []
+        gcs_blobs = bigquery.multi_upload_to_gcs(file_parts)
         for file_part in file_parts:
-            gcs_blobs.append(bigquery.upload_to_gcs(file_part))
             os.remove(file_part)
 
         # Creating temp table in Bigquery
