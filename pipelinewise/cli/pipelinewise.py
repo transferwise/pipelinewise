74789540575e6264e390b3e16a9f50caf43027cd
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-02-20 22:15:29 +0000

    Improve signal handling
    Only the `bash` process which PipelineWise creates needs to be
    terminated. Any other processes are not PipelineWise's responsibility to
    kill.

--- pipelinewise/cli/pipelinewise.py
+++ pipelinewise/cli/pipelinewise.py
@@ -1316,9 +1316,10 @@ class PipelineWise:
                 parent = psutil.Process(pid)
 
                 # Terminate child processes
-                for child in parent.children(recursive=True):
-                    self.logger.info('Sending SIGTERM to child pid %s...', child.pid)
-                    child.send_signal(signal.SIGTERM)
+                child = parent.children()[-1]
+                self.logger.info('Sending SIGTERM to child pid %s...', child.pid)
+                child.terminate()
+                child.wait()
 
             # Rename log files from running to terminated status
             if self.tap_run_log_file:
