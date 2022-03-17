9ca61ce6646a99d8c9ecd2f2e18f57050342e554
Amir Mofakhar <amir.mofakhar@wise.com>
2022-03-17 11:03:17 +0000

    Release 0.42.0 (#916)
    * release 0.42.0
    
    * Update setup.py
    
    Co-authored-by: Jeet Parekh <94441288+jeet-parekh-wise@users.noreply.github.com>
    
    * fixed changelog
    
    Co-authored-by: Jeet Parekh <94441288+jeet-parekh-wise@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='pipelinewise',
-      python_requires='>=3.7<3.10',
-      version='0.41.0',
+      python_requires='>=3.7,<3.10',
+      version='0.42.0',
       description='PipelineWise',
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
