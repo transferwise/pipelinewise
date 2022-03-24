f869e91775b2b9f93942e1e5e9a3e93bb70e7bb8
Jeet Parekh <jeet.parekh@transferwise.com>
2022-03-24 15:30:10 +0000

    [AP-1172] skip postgres tests if tap postgres credentials are not configured

--- tests/end_to_end/target_snowflake/tap_postgres/__init__.py
+++ tests/end_to_end/target_snowflake/tap_postgres/__init__.py
@@ -4,6 +4,8 @@ from tests.end_to_end.target_snowflake import TargetSnowflake
 class TapPostgres(TargetSnowflake):
     def setUp(self):
         super().setUp()
+        if self.e2e_env.env["TAP_POSTGRES"]["is_configured"] is False:
+            self.skipTest("TAP POSTGRES credentials are not configured")
 
     def tearDown(self):
         super().tearDown()
