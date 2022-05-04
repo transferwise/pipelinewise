d9930bc9d586c292de763e23276806d4aa09519a
Amir Mofakhar <amir.mofakhar@wise.com>
2022-05-04 16:35:23 +0100

    updated setup file for adding partial sync command

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(name='pipelinewise',
               'mongodb-to-snowflake=pipelinewise.fastsync.mongodb_to_snowflake:main',
               'mongodb-to-postgres=pipelinewise.fastsync.mongodb_to_postgres:main',
               'mongodb-to-bigquery=pipelinewise.fastsync.mongodb_to_bigquery:main',
+              'partial-mysql-to-snowflake=pipelinewise.fastsync.partialsync.mysql_to_snowflake:main'
           ]
       },
       packages=find_packages(exclude=['tests*']),
