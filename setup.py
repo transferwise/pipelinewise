fdb6bc083e3e8b09b79526a86b2b33565d4ba0d7
Amir Mofakhar <amir.mofakhar@wise.com>
2022-07-07 14:31:52 +0100

    release 0.4.47 (#987)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(name='pipelinewise',
       python_requires='>=3.7,<3.10',
-      version='0.46.0',
+      version='0.47.0',
       description='PipelineWise',
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
