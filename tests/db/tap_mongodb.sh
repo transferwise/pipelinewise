b5ec2744c79e5ebde32d5638d32f1f878432d5b6
Amir Mofakhar <amir.mofakhar@wise.com>
2025-06-03 19:28:36 +0100

    roll back tap_mongodb.sh

--- tests/db/tap_mongodb.sh
+++ tests/db/tap_mongodb.sh
@@ -27,7 +27,7 @@ fi
 URI="mongodb://${TAP_MONGODB_USER}:${TAP_MONGODB_PASSWORD}@${TAP_MONGODB_HOST}:${TAP_MONGODB_PORT}/${TAP_MONGODB_DB}?authSource=admin"
 
 mongoimport --uri ${URI} \
-  --ssl --tlsInsecure \
+  --ssl --sslAllowInvalidCertificates \
   --collection listings \
   --type csv \
   --headerline \
@@ -36,11 +36,12 @@ mongoimport --uri ${URI} \
 mongorestore \
   --host ${TAP_MONGODB_HOST} \
   --port ${TAP_MONGODB_PORT} \
-  --nsInclude ${TAP_MONGODB_DB}.my_collection \
+  --db ${TAP_MONGODB_DB}.my_collection \
   --authenticationDatabase admin \
   --username ${TAP_MONGODB_USER} \
   --password ${TAP_MONGODB_PASSWORD} \
-  --ssl --tlsInsecure \
+  --ssl --sslAllowInvalidCertificates \
+  --collection my_collection \
   --drop \
   --gzip \
   ${TEST_DB_DATA_2}
@@ -48,11 +49,12 @@ mongorestore \
 mongorestore \
   --host ${TAP_MONGODB_HOST} \
   --port ${TAP_MONGODB_PORT} \
-  --nsInclude ${TAP_MONGODB_DB}.all_datatypes \
+  --db ${TAP_MONGODB_DB}.all_datatypes \
   --authenticationDatabase admin \
   --username ${TAP_MONGODB_USER} \
   --password ${TAP_MONGODB_PASSWORD} \
-  --ssl --tlsInsecure \
+  --ssl --sslAllowInvalidCertificates \
+  --collection all_datatypes \
   --drop \
   --gzip \
   ${TEST_DB_DATA_3}
