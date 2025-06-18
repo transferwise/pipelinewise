166657df68ef6e26898902da85c66b0efa40eba3
mhassanzahid <hassan.zahid@transferwise.com>
2025-06-18 12:36:04 +0300

    bump connector version and add CHANGELOG.md entries

--- singer-connectors/target-snowflake/setup.py
+++ singer-connectors/target-snowflake/setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name="pipelinewise-target-snowflake",
-      version="2.3.0",
+      version="2.4.0",
       description="Singer.io target for loading data to Snowflake - PipelineWise compatible",
       long_description=long_description,
       long_description_content_type='text/markdown',
