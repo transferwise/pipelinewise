91ad5858001b302ea1947d35f835c42945c1263d
Amir Mofakhar <amir.mofakhar@wise.com>
2026-04-16 15:06:32 +0100

    Updates for Python 3.12

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='pipelinewise',
       install_requires=[
           'argparse==1.4.0',
           'tabulate==0.8.9',
-          'PyYAML==6.0',
+          'PyYAML==6.0.3',
           'ansible-core==2.17.8',
           'Jinja2==3.1.6',
           'joblib==1.3.2',
